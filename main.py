1ef2025d4de705f56179ed89786549b5153425c3
SamanRYM <yerolloy@gmail.com>
2025-07-05 22:52:37 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "📱 Добро пожаловать в OLXuz!\n\n🛒 Здесь вы можете найти и продать товары прямо в Telegram.",
+        parse_mode="Markdown"
+    )
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
